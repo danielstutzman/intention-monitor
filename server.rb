4f69a5a34a88a829639cfd633e18b9d398c34920
Dan Stutzman <dan@danstutzman.com>
2014-10-31 17:58:07 -0600

    Now tailing the nginx access log and storing events in influxdb.

--- server.rb
+++ server.rb
@@ -4,6 +4,7 @@ require 'thin'
 require 'dotenv'
 require 'em-http-request'
 require 'json'
+require 'em-ssh'
 
 Dotenv.load! '.env.apis'
 
@@ -44,6 +45,7 @@ EventMachine.run do
     signals: false, # so Ctrl-C works
   })
 
+  if false
   EventMachine.add_periodic_timer 5 do
     print 'Checking Papertrail...'
     token = ENV['PAPERTRAIL_TOKEN'] or raise "Need ENV[PAPERTRAIL_TOKEN]"
@@ -84,4 +86,59 @@ EventMachine.run do
       end
     end
   end
+  end
+
+  EventMachine::Ssh.start('basicruby.danstutzman.com', 'root',
+                          passphrase: File.read('passphrase').strip) do |conn|
+    conn.errback do |e|
+      STDERR.puts "#{e} (#{e.class})"
+    end
+    conn.callback do |ssh|
+      channel = ssh.open_channel do |ch|
+        command = 'docker exec 9bb32 tail -f /var/log/nginx/access.log'
+        ch.exec command do |ch, success|
+          #raise 'could not execute command' unless success
+          ch.on_data do |c, data| # (output to stdout)
+            data.split("\n").each do |line|
+              if match = line.match(%r@(?<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?<timestamp>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(?<method>[A-Z]+) )(?<path>[^ ]+) (http\/1\.[01]")) (?<status>\d{3}) (?<bytes>\d+) (["](?<referer>(\-)|(.+))["]) (["](?<useragent>.+)["])@i)
+                since_epoch = Time.parse(match[:timestamp].sub(':', ' ')).to_i
+                $stdout.puts match[:ipaddress], since_epoch
+                is_monitis = match[:useragent].include?('monitis')
+
+
+      print 'Posting to InfluxDB...'
+      url = 'http://localhost:8086/db/root/series?u=root&p=root&time_precision=s'
+      json = [{
+        name: 'nginx_access_logs',
+        columns: %w[time       hostname     ip_address         method
+                    path       status       bytes              is_monitis],
+        points: [[since_epoch, 'basicruby', match[:ipaddress], match[:method],
+                  match[:path], match[:status], match[:bytes], is_monitis]],
+      }].to_json
+      http = EventMachine::HttpRequest.new(url).post body: json
+      http.errback  do
+        STDERR.puts "#{http.response_header.status} from #{http.req.uri}:"
+        STDERR.puts http.response
+      end
+      http.callback do
+        puts 'done.'
+        if http.response != ''
+          STDERR.puts http.response
+        end
+      end
+
+
+              else
+                $stderr.puts "Doesn't match regex: #{line}"
+              end
+            end
+          end
+          ch.on_extended_data do |c, type, data| # (output to stderr)
+            $stderr.print data
+          end
+        end
+      end
+    end
+  end
+
 end
